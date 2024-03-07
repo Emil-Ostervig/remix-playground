d0db281acecc619fa0f24f5cca71f53a8ad37d0d
Emil Aasborg Østervig <eao@impact.dk>
2024-03-07 16:48:01 +0100

    Forward CMS headers

--- app/loaders/pageLoader.ts
+++ app/loaders/pageLoader.ts
@@ -1,14 +1,20 @@
-import { LoaderFunctionArgs } from '@remix-run/node';
+import { json, LoaderFunctionArgs } from '@remix-run/node';
 import { getPage } from '~/api/getPage';
 
 export const pageLoader = async ({ params, context: _context, request: _request }: LoaderFunctionArgs) => {
   const routeParam = params?.['*'] ?? '';
   const route = `/${routeParam}`;
-  let data;
   try {
-    data = await getPage({
+    const data = await getPage({
       route: `${route}`,
       locale: 'en',
+    });
+
+    return json(data, {
+      // This is just a mock of what the API would respons with.
+      headers: {
+        'Cache-Control': 'public, max-age=120, s-maxage=60, stale-while-revalidate=60, stale-while-error=3600',
+      },
     });
   } catch (e) {
     console.error(e);
@@ -17,6 +23,4 @@ export const pageLoader = async ({ params, context: _context, request: _request 
       statusText: 'whoops',
     });
   }
-
-  return data;
 };
--- app/routes/$.tsx
+++ app/routes/$.tsx
@@ -1,3 +1,4 @@
+import { HeadersFunction } from '@remix-run/node';
 import { MetaFunction, useLoaderData } from '@remix-run/react';
 import { DefaultLayout } from '~/components/Layout/DefaultLayout/DefaultLayout';
 import { ErrorBoundary as ErrorBoundaryLayout } from '~/components/Layout/ErrorBoundary/ErrorBoundary';
@@ -19,9 +20,28 @@ export const meta: MetaFunction<typeof loader> = ({ data }) => {
   return [{ title: data?.pageTitle ?? data?.meta?.title }];
 };
 
-export const headers = () => ({
-  'Cache-Control': 'public, max-age=300, must-revalidate',
-});
+export const headers: HeadersFunction = ({ loaderHeaders }) => {
+  const headers = new Headers();
+  // Headers to forward from CMS
+  const headersFromCms = [
+    'Cache-Control',
+  ]
+
+  for (const header of headersFromCms) {
+    if (loaderHeaders.has(header)) {
+      headers.set(header, loaderHeaders.get(header)!)
+    }
+  };
+  
+  // Custom headers added to the response
+  const customHeaders = {
+    'Some-Custom-Header': 'Some-Custom-Value'
+  }
+  for (const [key, value] of Object.entries(customHeaders)) {
+    headers.set(key, value)
+  }
+  return headers;
+};
 
 export const loader = pageLoader;
 export const ErrorBoundary = ErrorBoundaryLayout;

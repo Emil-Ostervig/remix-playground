8e1678309c3d39595f2c65e61aad83261fb28d3b
Emil Aasborg Østervig <eao@impact.dk>
2024-03-07 16:13:29 +0100

    Adjust headers

--- app/routes/$.tsx
+++ app/routes/$.tsx
@@ -4,15 +4,8 @@ import { ErrorBoundary as ErrorBoundaryLayout } from '~/components/Layout/ErrorB
 import { PageContext } from '~/context/PageContext/PageContext';
 import { pageLoader } from '~/loaders/pageLoader';
 
-export const meta: MetaFunction<typeof loader> = ({ data }) => {
-  return [{ title: data?.pageTitle ?? data?.meta?.title }];
-};
 
-export const headers = () => ({
-  'Cache-Control': 'public, max-age=300, stale-while-revalidate=60',
-});
 
-export const loader = pageLoader;
 export const Index = () => {
   const page = useLoaderData<typeof pageLoader>();
   return (
@@ -22,5 +15,14 @@ export const Index = () => {
   );
 };
 
+export const meta: MetaFunction<typeof loader> = ({ data }) => {
+  return [{ title: data?.pageTitle ?? data?.meta?.title }];
+};
+
+export const headers = () => ({
+  'Cache-Control': 'public, max-age=300, must-revalidate',
+});
+
+export const loader = pageLoader;
 export const ErrorBoundary = ErrorBoundaryLayout;
 export default Index;
